73e5351b3c69ada0b221707c905d887e64976029
Mario-bgt <mario_baumgartner@outlook.com>
2023-10-21 17:16:06 +0200

    ysoez

--- fuer_silvan/A5/A5_4.py
+++ fuer_silvan/A5/A5_4.py
@@ -1,23 +1,13 @@
 # This signature is required for the automated grading to work.
 # Do not rename the function or change its list of parameters.
+
+
 def analyze(posts):
-    index_dictionary = {}
-    for i in range(len(posts)):
-        posts[i] = posts[i].split()
-        for j in range(len(posts[i])):
-            if posts[i][j].startswith("#"):
-                if posts[i][j][1:] not in index_dictionary:
-                    index_dictionary[posts[i][j][1:]] = 1
-                else:
-                    index_dictionary[posts[i][j][1:]] += 1
-    return index_dictionary
-
-
-def analyze_simple(posts):
     index_dictionary = {}
     for i in posts:
         # make a whitespace infront of #:
         i = i.replace("#", " #")
+        i = "".join([c if c.isalnum() or c == "#" else " " for c in i])
         for j in i.split():
             if j.startswith("#") and len(j) > 1 and not j[1].isdigit():
                 if j[1:] not in index_dictionary:
@@ -35,6 +25,10 @@ posts = [
     "good morning #zurich #limmat",
     "spend my #weekend in #zurich",
     "#zurich <3 #nothing #236",
-    "Ohhhhhooo#IHATE #zurich"]
+    "Ohhhhhooo#IHATE #zurich",
+    "I love #zurich.com",
+    "I love #sbb.com",]
 # print(analyze(posts))
-print(analyze_simple(posts))
+print(analyze(posts))
+
+
