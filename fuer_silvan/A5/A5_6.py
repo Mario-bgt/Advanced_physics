ce38c49e1a418e30378200c29c034f2461ca6d8d
Mario-bgt <mario_baumgartner@outlook.com>
2023-10-21 21:55:00 +0200

    hw5 done

--- fuer_silvan/A5/A5_6.py
+++ fuer_silvan/A5/A5_6.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from unittest import TestCase
-from script import median
+from task.script import median
+
 
 # Extend the test suite with regression tests that cover the
 # meaningful bug reports. Make sure that you define test methods
@@ -8,7 +9,7 @@ from script import median
 # body, or -otherwise- the grading will mark the test suite as
 # invalid.
 class MedianTests(TestCase):
-    def testuser1(self):
+    def test_1(self):
         """User 1: Super annoying! I collect all my bills in a list, only yesterday,
         I paid 5.90 for my Latte Macchiato. When I use your function to calculate
         the median, it simply does not work!? It returns numbers that are not even
@@ -18,7 +19,7 @@ class MedianTests(TestCase):
         expected = 3.95
         self.assertEqual(actual, expected)
 
-    def testuser2v1(self):
+    def test_2(self):
         """User 2: The median is defined for two cases. If "the middle" points to an
         actual index, it should be used, but when it falls "between" two values,
         the average should be used."""
@@ -27,8 +28,7 @@ class MedianTests(TestCase):
         expected = 2.5
         self.assertEqual(actual, expected)
 
-
-    def testuser2v2(self):
+    def test_3(self):
         """User 2: The median is defined for two cases. If "the middle" points to an
         actual index, it should be used, but when it falls "between" two values,
         the average should be used."""
@@ -37,3 +37,11 @@ class MedianTests(TestCase):
         expected = 3
         self.assertEqual(actual, expected)
 
+    def test_4(self):
+        """User 4: Just because I don't have enough values in my list does not mean
+        that your function can crash. It happens. Every. Time. I am furious! Wouldn't
+        'None' be a good default?"""
+        testlist = []
+        actual = median(testlist)
+        expected = None
+        self.assertEqual(actual, expected)
--- fuer_silvan/A5/A5_7.py
+++ fuer_silvan/A5/A5_7.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """Um alle Werte oben und unten gleichzumachen, sollten die gegebenen Dominosteine eine ausreichende Anzahl gleicher
- Werte haben. Wir müssen also die Häufigkeit der Werte auf der Ober- und Unterseite vergleichen. Wenn genügend, sagen
-  wir 2er, auf der Unterseite vorhanden sind um die Bedingung zu erfüllen, können wir true zurückgeben. Ausserdem
-   sollten wir die Dominosteine mit doppelten Werten berücksichtigen. Es ist möglich, dass eine Häufigkeit von
-   irgendeinem Wert zeigt, dass wir genug geeignete Dominosteine haben, aber sie können ein Teil der Duplikaten sein.
-   Wir müssen also solche Duplikate zählen. In unserem Fall erstellen wir einen eigenen Zähler für jeden Wert ohne
-   Duplikate."""
+Werte haben. Wir müssen also die Häufigkeit der Werte auf der Ober- und Unterseite vergleichen. Wenn genügend, sagen
+wir 2er, auf der Unterseite vorhanden sind um die Bedingung zu erfüllen, können wir true zurückgeben. Ausserdem
+sollten wir die Dominosteine mit doppelten Werten berücksichtigen. Es ist möglich, dass eine Häufigkeit von
+irgendeinem Wert zeigt, dass wir genug geeignete Dominosteine haben, aber sie können ein Teil der Duplikaten sein.
+Wir müssen also solche Duplikate zählen. In unserem Fall erstellen wir einen eigenen Zähler für jeden Wert ohne
+Duplikate."""
 
 
 # This signature is required for the automated grading to work.
@@ -21,7 +21,9 @@ def min_domino_rotations(top, bottom):
         else:
             top_counter[topnum-1] += 1
             bottom_counter[bottomnum-1] += 1
-
+    print(top_counter)
+    print(bottom_counter)
+    print(total_counter)
     for i in range(6):
         if total_counter[i] - top_counter[i] == bottom_counter[i]:
             return min(top_counter[i], bottom_counter[i])
